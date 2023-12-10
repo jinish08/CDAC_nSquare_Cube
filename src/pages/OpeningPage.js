c4f5b0f92f10dcef6acf82324ce9f4f79711fd11
Ayush Shah <shahayush934@gmail.com>
2023-12-10 14:28:21 +0530

    positioning change

--- src/pages/OpeningPage.js
+++ src/pages/OpeningPage.js
@@ -8,10 +8,10 @@ const OpeningPage = () => {
         <div className="text-7xl" style={{ position: 'fixed', textAlign: 'center', top: '7%', zIndex: +1, fontFamily: "cursive" }}>
             <h1>Sum of Squares of N Natural Numbers</h1>
         </div>
-        <div style={{ position: 'fixed', bottom: '20%', left: '33%'}}>
+        <div style={{ position: 'fixed', bottom: '20%', left: '32%'}}>
             <img style={{width:'100%'}} src={'./images/character.png'} alt='character'/>
         </div>
-        <div className="fixed bg-gray-300/80 rounded-xl backdrop-blur-md" style={{ zIndex: 999 , left: "50%", bottom: "45%", width: "30%"}}>
+        <div className="fixed bg-gray-300/80 rounded-xl backdrop-blur-md" style={{ zIndex: 999 , left: "51%", bottom: "45%", width: "30%"}}>
             <div className="flex flex-col m-4">
                 <p className="text-3xl">
                     Lets learn about the sum of squares of n natural numbers formula and it's derivation.
